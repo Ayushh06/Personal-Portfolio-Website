93cd8e0e3217bda178aa6765328437354dabeac9
Ayush Varshney <139785829+Ayushh06@users.noreply.github.com>
2026-06-27 16:51:36 +0530

    Change typing text from 'Frontend Developer' to 'Python Developer'
    Updated the string 'Frontend Developer' to 'Python Developer' in the Typed.js configuration.

--- Script.js
+++ Script.js
@@ -4,7 +4,7 @@ menuIcon.onclick = () => {
 navLinks.classList.toggle('active');
 }
 var typed = new Typed("#typing-text", {
-    strings: ["MCA Undergrad", "Frontend Developer", "Frontend Developer", "Coder"],
+    strings: ["MCA Undergrad", "Python Developer", "Frontend Developer", "Coder"],
     typeSpeed: 80,
     backSpeed: 50,
     backDelay: 1200,
