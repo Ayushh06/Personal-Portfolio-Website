64ee4b262fd09259ded641353cdf4eebbb0a55d4
Ayush Varshney <139785829+Ayushh06@users.noreply.github.com>
2026-06-27 12:56:40 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,12 @@
+ const menuIcon = document.querySelector('#menu-icon');
+const navLinks = document.querySelector('.nav-links');
+menuIcon.onclick = () => {
+navLinks.classList.toggle('active');
+}
+var typed = new Typed("#typing-text", {
+    strings: ["MCA Undergrad", "Frontend Developer", "Coder"],
+    typeSpeed: 80,
+    backSpeed: 50,
+    backDelay: 1200,
+    loop: true
+});
